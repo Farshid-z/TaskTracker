e3726d966ef49210fb391b8c7f9210bba978ca5b
Farshid Zeynalzadeh <f.zeinalzadeh83@gmail.com>
2024-09-07 23:53:35 +0330

    add FileHelper

--- Command.cs
+++ Command.cs
@@ -1,15 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TaskTracker.Entities;
 
 namespace TaskTracker
 {
     class Command 
     {
-        public void Add(string param)
+        public void Add(string description)
         {
-
+            Task task = new Task();
+            task.Description = description;
+            task.CreatedAt = DateTime.Now;
+            task.UpdatedAt = DateTime.Now;
         }
+        public int GetNewId()
+        {
+            
+        }
+
         public void Update()
         {
 
---
+++ FileHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace TaskTracker
+{
+    class FileHelper
+    {
+        static string CurrentPath = Directory.GetCurrentDirectory();
+        public static void AddFile(string fileName)
+        {
+            if (string.IsNullOrEmpty(fileName))
+                return;
+            string file = CurrentPath + fileName ;
+            if(File.Exists(fileName))
+            try
+            {
+                using (FileStream fileStream  = File.Create(fileName))
+                {
+                    
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
